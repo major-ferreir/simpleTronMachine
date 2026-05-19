343c95d0b449aafeb3f426fd53c54486c7984a6b
major-ferreir <edmiro952@gmail.com>
2026-05-19 22:57:48 +0100

    Classes do projecto

---
+++ SimpleTron/src/classes/Computador.java
@@ -0,0 +1,17 @@
+package classes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eferreir
+ */
+
+public class Computador {
+    private Memoria memoria;
+    private Registadores regs;
+    //
+}
